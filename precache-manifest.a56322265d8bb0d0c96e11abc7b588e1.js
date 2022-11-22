6fc7a291a4f065eea4a559cfe32f40efdcf54074
Trong Tran <tdtrong1507@gmail.com>
2022-11-22 18:22:32 +0700

    Updates

---
+++ precache-manifest.a56322265d8bb0d0c96e11abc7b588e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5690c65954afbe62da0bd27b8842b809",
+    "url": "/movie_website_project/index.html"
+  },
+  {
+    "revision": "cc9c1b98e32890dbb71c",
+    "url": "/movie_website_project/static/css/main.851e57c9.chunk.css"
+  },
+  {
+    "revision": "566e6304faca6f937606",
+    "url": "/movie_website_project/static/js/2.16842206.chunk.js"
+  },
+  {
+    "revision": "a670d5f8533378d00773d4f0433ae4e4",
+    "url": "/movie_website_project/static/js/2.16842206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9c1b98e32890dbb71c",
+    "url": "/movie_website_project/static/js/main.b926e103.chunk.js"
+  },
+  {
+    "revision": "384ff80cebdcfe7b8cf3",
+    "url": "/movie_website_project/static/js/runtime-main.51a706f2.js"
+  }
+]);
